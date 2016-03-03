3cea7c9d2f9f4f080461f61431196bdc630f15e9
Charles Hathaway <chathaway@logrit.com>
2016-03-02 21:12:35 -0500

    Added in control for repo permission
    They don't control anything at the moment...

--- gitcentral/views.py
+++ gitcentral/views.py
@@ -1,8 +1,10 @@
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
+from django.core.exceptions import PermissionDenied
 from django.core.urlresolvers import reverse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
+from django.views.generic.detail import SingleObjectMixin
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
 
 from .models import *
@@ -22,15 +24,22 @@ class AllRepoListView(ListView):
     def get_queryset(self):
 	if self.request.user.is_authenticated():
 		permissions = RepoPermission.objects.filter(owner=self.request.user)
-		return Repo.objects.filter(Q(public=True) | Q(repopermission__in=permissions))
+		return Repo.objects.filter(Q(public=True) | Q(repopermission__in=permissions)).distinct()
 	return Repo.objects.filter(public=True)
 
 class RepoListView(ListView):
     model = Repo
 
+    def  get_context_data(self, **kwargs):
+        context = super(RepoListView, self).get_context_data(**kwargs)
+	context['repo_user'] = get_object_or_404(User, username=self.kwargs['username'])
+	return context
+
     def get_queryset(self):
-	permissions = RepoPermission.objects.filter(owner=self.request.user)
-	return Repo.objects.filter(Q(owner=self.request.user), Q(public=True) | Q(repopermission__in=permissions))
+	permissions = RepoPermission.objects.filter(owner=get_object_or_404(User, username=self.kwargs['username']))
+	if self.request.user.is_authenticated():
+	    return Repo.objects.filter(Q(public=True) | Q(repopermission__in=permissions)).distinct()
+	return Repo.objects.filter(public=True)
 
 class RepoCreateView(CreateView):
     model = Repo
@@ -41,6 +50,38 @@ class RepoCreateView(CreateView):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super(RepoCreateView, self).form_valid(form)
+
+class RepoPermissionListView(ListView):
+    model = RepoPermission
+
+    def get_context_data(self, **kwargs):
+        context = super(RepoPermissionListView, self).get_context_data(**kwargs)
+	context['repo'] = get_object_or_404(Repo, pk=self.kwargs['pk'])
+	return context
+
+    def get_queryset(self):
+	return RepoPermission.objects.filter(repo=get_object_or_404(Repo, pk=self.kwargs['pk']))
+
+class RepoPermissionCreateView(CreateView):
+    model = RepoPermission
+    fields = ('owner', 'permission',)
+
+    def get_success_url(self):
+        return reverse('permission-list', kwargs={"pk": self.object.repo.pk})
+
+    def form_valid(self, form):
+        form.instance.repo = get_object_or_404(Repo, pk=self.kwargs['pk'])
+	if RepoPermission.objects.filter(repo=form.instance.repo, owner=form.instance.owner).count() != 0:
+	    raise PermissionDenied()
+        return super(RepoPermissionCreateView, self).form_valid(form)
+
+class RepoPermissionDeleteView(DeleteView):
+    model = RepoPermission
+    def get_success_url(self):
+        return reverse('permission-list', kwargs={"pk": self.object.repo.pk})
+    def get_object(self):
+    	repo_permission = get_object_or_404(RepoPermission, owner=get_object_or_404(User, pk=self.kwargs['user_id']), repo=get_object_or_404(Repo, pk=self.kwargs['repo_id']))
+        return repo_permission
 
 class KeyListView(ListView):
     model = Key
